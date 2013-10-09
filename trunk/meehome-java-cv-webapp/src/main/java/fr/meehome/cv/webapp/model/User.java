4d919493300dbb9056c396ed283cf1b31be5ae92
raballand.romain@gmail.com <raballand.romain@gmail.com@0ab24f9b-b1d4-39bb-7896-ff154f87bd9b>
2013-10-09 07:08:57 +0000

    ajout d'un breadcrumbs separation du controlleur de connexion modification des libelles de properties

--- trunk/meehome-java-cv-webapp/src/main/java/fr/meehome/cv/webapp/model/User.java
+++ trunk/meehome-java-cv-webapp/src/main/java/fr/meehome/cv/webapp/model/User.java
@@ -4,47 +4,47 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 public class User {
 
-	@NotEmpty(message = "Veuillez renseigner votre email")
-	private String email;
+    @NotEmpty
+    private String email;
 
-	@NotEmpty(message = "Veuillez renseigner votre password")
-	private String password;
+    @NotEmpty
+    private String password;
 
-	@NotEmpty(message = "Veuillez renseigner votre nom")
-	private String nom;
+    @NotEmpty
+    private String nom;
 
-	@NotEmpty(message = "Veuillez renseigner votre prenom")
-	private String prenom;
+    @NotEmpty
+    private String prenom;
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 }
