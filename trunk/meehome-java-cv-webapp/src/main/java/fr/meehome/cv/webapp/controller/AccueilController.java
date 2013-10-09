6c64ccfba8b12a05f770912e598b86114418d530
raballand.romain@gmail.com <raballand.romain@gmail.com@0ab24f9b-b1d4-39bb-7896-ff154f87bd9b>
2013-10-09 07:08:47 +0000

    ajout d'un breadcrumbs separation du controlleur de connexion modification des libelles de properties

--- trunk/meehome-java-cv-webapp/src/main/java/fr/meehome/cv/webapp/controller/AccueilController.java
+++ trunk/meehome-java-cv-webapp/src/main/java/fr/meehome/cv/webapp/controller/AccueilController.java
@@ -2,10 +2,10 @@ package fr.meehome.cv.webapp.controller;
 
 import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,11 +18,16 @@ import fr.meehome.cv.webapp.utils.Pages;
 @RequestMapping("/accueil/")
 public class AccueilController {
 
-    // private static final Logger logger =
-    // Logger.getLogger(AccueilController.class);
+    private static final Logger LOG = Logger.getLogger(AccueilController.class);
 
+    /**
+     * Gestion des bean affichés dans la page
+     * 
+     * @return
+     */
     @ModelAttribute("authentification")
     public Authentification getAuthentification() {
+        LOG.debug("AccueilController :: getAuthentification");
         return new Authentification();
     }
 
@@ -32,29 +37,9 @@ public class AccueilController {
         return Pages.accueil.getLibelle();
     }
 
-    // VALIDATION FORMULAIRE DE CONNEXION
-    @RequestMapping(value = "/connexion", method = RequestMethod.POST)
-    public String connexion(@Valid
-    Authentification authentification, BindingResult result, ModelMap model) {
-        if (!result.hasErrors()) {
-            if (!authentification.getEmail().equals("raballand.romain@gmail.com")
-                            || !authentification.getPassword().equals("13041983")) {
-                result.addError(new ObjectError("authentification.failed",
-                                "Votre login ou votre mot de passe n'est pas valide"));
-            } else {
-                User user = new User();
-                user.setNom("RABALLAND");
-                user.setPrenom("Romain");
-                model.addAttribute("userAuthentificated", user);
-            }
-        }
-        return Pages.accueil.getLibelle();
-    }
-
     // AFFICHE LA PAGE D'INSCRIPTION
     @RequestMapping(value = "/inscription", method = RequestMethod.GET)
     public String inscription(ModelMap model) {
-        model.addAttribute("authentification", new Authentification());
         model.addAttribute("user", new User());
         return Pages.inscription.getLibelle();
     }
